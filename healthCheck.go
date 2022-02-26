82b2b4f700e0c2558ae0fafa71fdbfbcc0dfbfe3
OmkoBass <omkob@hotmail.com>
2022-02-26 17:02:02 +0100

    Health check checks different servers based on date

--- healthCheck.go
+++ healthCheck.go
@@ -8,7 +8,19 @@ import (
 )
 
 func startHealthCheck(numberOfSeconds int) {
-	// Check each server if it's alive every 5 seconds
+	currentTime := time.Now()
+
+	var backendList []*server
+
+	// Health check first server list
+	// if day in the month is < 15
+	if currentTime.Day() < 15 {
+		backendList = serverListFirst
+	} else {
+		backendList = serverListSecond
+	}
+
+	// Check each server if it's alive every n seconds
 	scheduler := gocron.NewScheduler(time.Local)
 
 	for _, backend := range backendList {
